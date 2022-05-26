b119dbf018b97a8d949d91e56f6beb206bc03bb5
Vitor Basilio <106208107+VitorBR162@users.noreply.github.com>
2022-05-25 21:26:02 -0300

    altereii

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
  echo "Ola meu amor";
 
- echo "eu te amo mais que tudo nessa vida"; 
+ echo "eu te amo mais que tudo nessa v13123123ida"; 
 
- echo "olar vitor bao demais?";|
+ echo "olar vitor bao dema12312is?";|
 
- echo "Oi Vitor"
+ echo "Oi Vito3123123r"
 
 >?
